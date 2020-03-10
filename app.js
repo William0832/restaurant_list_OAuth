d434118eb2cf750edb082cb17b77f6d56c55685b
William0832 <squall0832@gmail.com>
2020-03-11 03:58:24 +0800

    add fb login and add connect-flash for UI messages

--- app.js
+++ app.js
@@ -1,65 +1,57 @@
 const express = require('express')
 const app = express()
+
+// 判別開發環境
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('passport')
+const flash = require('connect-flash')
 const port = 3000
 
-// find static file directory
 app.use(express.static('public'))
-
-// 載入 method-override & 設定
-const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
-
-// 載入 handlebars as template engine & set layout: main
-const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayouts: 'main' }))
 app.set('view engine', 'handlebars')
-
-// 載入 body-parser & setting it
-const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// 載入 mongoose & 連線 DB & 使用 connection 物件
-const mongoose = require('mongoose')
-// 載入 session
-const session = require('express-session')
-// 載入 passport
-const passport = require('passport')
-
-// mongoDB 連線
 mongoose.connect('mongodb://localhost/restaurant', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 })
 const db = mongoose.connection
-// db error
 db.on('error', () => {
   console.log('db error')
 })
-// db connected
 db.once('open', () => {
   console.log('db connected!')
 })
 
 app.use(
   session({
-    secret: '5566', // secret: 定義一組屬於你的字串做為私鑰
+    secret: '5566',
     resave: false,
     saveUninitialized: true
   })
 )
 
-// 使用 Passport
 app.use(passport.initialize())
 app.use(passport.session())
 
-// 載入 Passport config
 require('./config/passport')(passport)
 
-// 登入後可以取得使用者的資訊方便我們在 view 裡面直接使用
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.user = req.user
-  res.locals.isAuthenticated = req.isAuthenticated() // 辨識使用者是否已經登入的變數，讓 view 可以使用
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
@@ -67,6 +59,8 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/home.js'))
 app.use('/restaurants', require('./routes/restaurant.js'))
 app.use('/users', require('./routes/user.js'))
+app.use('/auth', require('./routes/auths'))
+
 // listen app
 app.listen(port, () => {
   console.log(`App is listening on localhost:${port}`)
