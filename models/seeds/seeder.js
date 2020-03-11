26bfed82f3aab01b9aaadb6ad0133db288e97827
William0832 <squall0832@gmail.com>
2020-03-11 08:56:46 +0800

    add seeder

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const Restaurant = require('../restaurant.js')
-const dataList = require('../../restaurant.json').results
+const User = require('../user.js')
+const restaurantData = require('../../restaurant.json').results
+
 mongoose.connect('mongodb://localhost/restaurant', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -13,9 +16,42 @@ db.on('error', () => {
 })
 
 db.once('open', () => {
-  console.log('db connected!')
-  for (const item of dataList) {
-    Restaurant.create(item)
+  const user = []
+  user.push(
+    new User({
+      name: 'user1',
+      email: 'user1@example.com',
+      password: '12345678'
+    })
+  )
+  user.push(
+    new User({
+      name: 'user2',
+      email: 'user2@example.com',
+      password: '12345678'
+    })
+  )
+
+  user.forEach(newUser => {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) return console.log(err)
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) return console.log(err)
+        newUser.password = hash
+        newUser.save(err => {
+          if (err) console.log(err)
+        })
+      })
+    })
+  })
+
+  restaurantData.forEach(restaurant => delete restaurant.id)
+  for (let i = 1; i <= restaurantData.length; i++) {
+    if (i <= 3) restaurantData[i].userId = user[0]._id
+    else if (i >= 4 && i <= 6) restaurantData[i].userId = user[1]._id
+    else break
+
+    Restaurant.create(restaurantData[i])
   }
   console.log('done')
 })
--- routes/user.js
+++ routes/user.js
@@ -68,7 +68,6 @@ router.post('/register', (req, res) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
             if (err) throw err
             newUser.password = hash
-
             newUser
               .save()
               .then(user => {
